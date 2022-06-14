057cc861bae4a06087c7f8f03d688acfb02f1a44
JhonCorzo <jhonwilliamcs@ufps.edu.co>
2022-06-13 22:29:03 -0500

    login funcionando

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php 
+//conexion a la base de datos y el servidor
+$conexion=mysqli_connect("localhost","root","","riesgos") or die("<h2>Error al conectar el servidor y la base de datos  </h2>");;
+/*
+$db = mysql_select_db("riesgos",$link) or die ("<h2>Error de conexion a la base de datos</h2>");
+*/
+
+
+
+
+ ?>
